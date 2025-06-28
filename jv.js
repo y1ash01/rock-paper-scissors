94f766382040c183f0648edddbde561562f3124e
yash <216164228+y1ash01@users.noreply.github.com>
2025-06-29 02:51:09 +0530

    added user interface for the game

--- jv.js
+++ jv.js
@@ -19,53 +19,70 @@ let computerScore=0;
 
 function playRound(humanChoice,computerChoice){
     humanChoice=humanChoice.toLowerCase()
+    let score = document.querySelector("#score");
     if (humanChoice=='rock'){
         if(computerChoice=='rock'){
-            console.log('tie')
+            score.textContent = `ROCK VS ROCK : TIE. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else if (computerChoice=='paper'){
-            console.log('loss')
             computerScore+=1;
+            score.textContent = `ROCK VS PAPER : LOSS. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else{
-            console.log('win')
             humanScore+=1;
+            score.textContent = `ROCK VS SCISSORS : WIN. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
     }
     if (humanChoice=='paper'){
         if(computerChoice=='paper'){
-            console.log('tie')
+            score.textContent = `PAPER VS PAPER : TIE. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else if (computerChoice=='rock'){
-            console.log('win')
             humanScore+=1;
+            score.textContent = `PAPER VS ROCK : WIN. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else{
-            console.log('loss')
             computerScore+=1;
+            score.textContent = `PAPER VS SCISSORS : LOSS. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
     }
     if (humanChoice=='scissor'){
         if(computerChoice=='scissor'){
-            console.log('tie')
+            score.textContent = `SCISSORS VS SCISSORS : TIE. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else if (computerChoice=='paper'){
-            console.log('win')
             humanScore+=1;
+            score.textContent = `SCISSORS VS PAPER : WIN. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
         else{
-            console.log('loss')
             computerScore+=1;
+            score.textContent = `SCISSORS VS ROCK : LOSS. Computer score: ${computerScore} | Human score: ${humanScore}`;
         }
     }
-}
-
-function playGame(){
-    for(let i=1;i<=5;i++){
-        playRound(getHumanChoice(),getComputerChoice())
-        console.log("your score: " + humanScore)
-        console.log("computer score: " + computerScore)
+    if (humanScore===5 || computerScore===5){
+        let result = document.querySelector('#result');
+        result.textContent = humanScore===5?"YOU WIN THE GAME":"YOU LOSE THE GAME";
+        humanScore=0;
+        computerScore=0;
+    }
+    else{
+        let result = document.querySelector('#result');
+        result.textContent = '';
     }
 }
 
-playGame()
+let btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn)=>{
+    btn.addEventListener("click",(e)=>{
+        if(btn.textContent === "ROCK"){
+            playRound('rock',getComputerChoice());
+        }
+        else if(btn.textContent === "PAPER"){
+            playRound('PAPER',getComputerChoice());
+        }
+        else{
+            playRound("scissor",getComputerChoice());
+        }
+    })
+});
